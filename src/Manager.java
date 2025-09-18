a936880254020839811c8068b0be54ea3b113136
Grace MUGWANEZA <gracemugwaneza018@gmail.com>
2025-09-18 16:31:01 +0200

    using try and catch

--- src/Manager.java
+++ src/Manager.java
@@ -2,23 +2,44 @@ class Manager extends Programmer{
     double  bonus;
     private double totalSalary;
 
+
     public Manager() {
 
-        bonus= 5000;
+        bonus = 5000;
     }
-    double baseSalary;
-    @Override
-    public double calculateSalary() {
-        baseSalary= super.calculateSalary();
 
-        totalSalary= baseSalary+ bonus;
+        double baseSalary;
+        @Override
 
-       return totalSalary;
-    }
-    @Override
-    public void displayInfo() {
-           System.out.println( "the total salary after adding salary  is " + calculateSalary());
+        public double calculateSalary () {
+            try{
+            baseSalary = super.calculateSalary();
+
+            totalSalary = baseSalary + bonus;
+
+            return totalSalary;
         }
+
+        catch(Exception e){
+            System.out.println("Error occurred while calculating salary");
+        }
+            return 0;
+        }
+        @Override
+
+        public void displayInfo () {
+            try{
+            System.out.println("the total salary after adding salary  is " + calculateSalary());
+        }
+        catch(Exception e){
+            System.out.println("error occurred on the output of salary");
+        }
+    }
+
+
+
+
+
 
 
 
