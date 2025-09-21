f876901f53a8faaed014a47989f1e00149774937
Grace MUGWANEZA <gracemugwaneza018@gmail.com>
2025-09-21 20:54:39 +0200

    removing the item

--- src/InventoryManagement.java
+++ src/InventoryManagement.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class InventoryManagement{
     public static void main(String[] args){
         ArrayList<Item> inventory= new ArrayList<>();
-        inventory.add(new Item(1,"HP", 1200, "Lptops"));
+        inventory.add(new Item(1,"HP", 1200, "Laptops"));
         inventory.add (new Item(2,"Lacoste", 2500, "T-shirts"));
         inventory.add(new Item(2, "Nike", 3000, "shoes"));
         inventory.add(new Item(4,"Techno",1500, "phones"));
@@ -20,5 +20,21 @@ public class InventoryManagement{
             }
         }
         System.out.println("The most expensiveItem: " + mostExpensive);
+
+        int removedId=2;// remove item with Id =2
+        Item toRemove=null;
+        for (Item item: inventory){
+            if (item.id==removedId){
+                toRemove=item;
+
+            }
+
+        }
+        if(toRemove != null){
+            inventory.remove(toRemove);
+        }
+
+
+
     }
 }
